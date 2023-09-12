c78f62e6ac9ef95f811c4df14bd6a33d89eff0f3
Camille Ivins <camillemivins@gmail.com>
2023-09-12 14:08:17 -0600

    env.js and axios geared up

---
+++ src/models/Gift.js
@@ -0,0 +1,12 @@
+export class Gift {
+    constructor(data) {
+        this.id = data.id
+        this.tag = data.tag
+        this.opened = data.opened
+        this.creatorId = data.creatorId
+        this.url = data.url
+        this.createdAt = data.createdAt
+        this.updatedAt = data.updatedAt
+    }
+
+}
